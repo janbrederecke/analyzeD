e3e8dfc8bf068472f0dcfa1a582962684e9f1f60
Jan Brederecke <58463668+janbrederecke@users.noreply.github.com>
2022-06-03 23:06:13 +0200

    Playing with the startup message.

--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,10 @@
     "\n",
     "== Thank you for using analyzeD =========================================",
     "\nIf you encounter any bugs feel free to file an issue at:",
-    "\n https://github.com/janbrederecke/analyzeD"
+    "\nhttps://github.com/janbrederecke/analyzeD",
+    "\n",
+    "\nYou might want to check my other packages at:",
+    "\nhttps://github.com/janbrederecke"
   )
   
   packageStartupMessage(msg)
